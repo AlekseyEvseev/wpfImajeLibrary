ba0efbdaffbe9a3bcd24a1629e9b8a5f5b20831e
Алексей <evseev_aleksey93@mail.ru>
2020-09-15 02:10:38 +0300

    Добавьте файлы проекта.

---
+++ wpfImajeLibrary/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace wpfImajeLibrary
+{
+  public  class Product
+    {
+       public string Articul { get; set; }
+       public string Name { get; set; }
+        public Image img;
+        public List<Image> Images { get; set; }
+
+    }
+}
